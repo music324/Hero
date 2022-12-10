3df78d84d23b64fa1b6e3e4fd409ee131ebc5e20
music324 <120071765+music324@users.noreply.github.com>
2022-12-10 13:11:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ if os.path.exists("Internal"):
 # VARS
 
 get_queue = {}
-BOT_TOKEN = getenv("BOT_TOKEN")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN", "5834312062:AAGgwcjbP5bkphbyeKeIgBkdHJhJrsaW4co")
+API_ID = int(getenv("API_ID", "19275563"))
+API_HASH = getenv("API_HASH", "332213ccd9f10bd2924e4824172e791e")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "900"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5908792351").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5908792351").split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001862187178"))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "-●⏤͟͟͞͞ ꜱᴘᴏᴛɪꜰʏ ✗ ᴍᴜsɪᴄ🕊️⃝🫧")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
@@ -26,14 +26,14 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
-if str(getenv("SUPPORT_CHANNEL")).strip() == "":
+if str(getenv("SUPPORT_CHANNEL")).strip() == "https://t.me/StarbotUpdate":
     SUPPORT_CHANNEL = None
 else:
     SUPPORT_CHANNEL = str(getenv("SUPPORT_CHANNEL"))
 if str(getenv("SUPPORT_GROUP")).strip() == "":
     SUPPORT_GROUP = None
 else:
-    SUPPORT_GROUP = str(getenv("SUPPORT_GROUP"))
+    SUPPORT_GROUP = str(getenv("SUPPORT_GROUP", "https://t.me/Super_Star_Singing_Group"))
 
 
 if str(getenv("STRING_SESSION1")).strip() == "":
